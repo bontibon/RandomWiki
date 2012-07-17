9acd6954ff81299167dc23f040f54ad6c9d1100c
Tim Cooper <tim.cooper@layeh.com>
2012-07-17 12:37:50 -0300

    Various changes.  - Changed name  - Added more languages  - DRYed the menu generation

--- RandomWikiEveryHour.user.js
+++ RandomWikiEveryHour.user.js
@@ -1,27 +1,56 @@
 // ==UserScript==
-// @name        RandomWikiEveryHour
+// @name        RandomWiki
 // @namespace   techietim
 // @description Opens a Wikipedia article at random intervals
 // @updateURL   https://github.com/techietim/RandomWikiEveryHour/raw/master/RandomWikiEveryHour.user.js
 // @include     http://*
 // @include     https://*
-// @version     0.1.1
+// @version     0.2
 // ==/UserScript==
 
-GM_registerMenuCommand("RandWiki: English", function(e)
+var languages = {
+    en: 'English',
+    fr: 'French',
+    es: 'Spanish',
+    ja: 'Japanese',
+    de: 'German',
+    pl: 'Polish',
+    zh: 'Chinese',
+    pt: 'Portuguese',
+    it: 'Italian',
+    ru: 'Russian'
+};
+
+var change_value = function(new_value)
 {
-    GM_setValue("language", "en");
+    var old_value = GM_getValue("language", "en");
+    GM_setValue("language", new_value);
+    alert(GM_info.script.name + "  Article language changed to " +
+        languages[new_value] + " from " + languages[old_value] + ".");
+};
+
+var showArticle = function()
+{
+    var language = GM_getValue("language", "en");
+    GM_openInTab("https://" + language + ".wikipedia.org/wiki/Special:Random");
+};
+
+GM_registerMenuCommand(GM_info.script.name + ": Show random article", function()
+{
+    showArticle();
 });
 
-GM_registerMenuCommand("RandWiki: French", function(e)
+for(var lang in languages)
 {
-    GM_setValue("language", "fr");
-});
-
-GM_registerMenuCommand("RandWiki: Spanish", function(e)
-{
-    GM_setValue("language", "es");
-});
+    GM_registerMenuCommand(GM_info.script.name + ": " + languages[lang],
+    (function(){
+        var lang_value = lang;
+        return function()
+        {
+            change_value(lang_value);
+        }
+    })());
+}
 
 var checkTime = function()
 {
@@ -31,9 +60,8 @@ var checkTime = function()
     
     if(stamp - last_update >= (Math.random() * 59 + 1) * 60000)
     {
-        var language = GM_getValue("language", "en")
         GM_setValue("last_update", stamp.toString());
-        GM_openInTab("https://" + language + ".wikipedia.org/wiki/Special:Random");
+        showArticle();
     }
 };
 checkTime();
